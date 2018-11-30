eceba46d6028d039fcee027407a920f0959033b3
Jonathan Stoye <mail@jonathanstoye.de>
2018-11-30 21:18:37 +0100

    feat: basic ui setup with material-ui

--- framework-ui/src/App.js
+++ framework-ui/src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from "react";
 import api from "./api";
 
 class App extends Component {
+  state = {
+    entries: []
+  };
+
   async componentDidMount() {
     const response = await api.getEntires();
     const data = await response.json();
     console.log(data["@graph"]);
+    this.setState({ entries: data["@graph"] });
   }
 
   render() {
-    return <div className="App" />;
+    const { entries } = this.state;
+    const content = entries.map(({ prefLabel }) => <li>{prefLabel.value}</li>);
+    return (
+      <div className="App">
+        <ul>{content}</ul>
+      </div>
+    );
   }
 }
 
